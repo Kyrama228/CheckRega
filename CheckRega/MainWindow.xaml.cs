a3c5511651c5d08f191653e6f0fa27b8669b2e22
leonidmi5a1@gmail.com <leonidmi5a1@gmail.com>
2022-05-25 06:15:34 +0300

    Добавьте файлы проекта.

---
+++ CheckRega/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckRega
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static RykobludEntities ent = new RykobludEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RegBtn.Visibility = Visibility.Hidden;
+            if(String.IsNullOrWhiteSpace(LoginTxt.Text) || String.IsNullOrWhiteSpace(PassPb.Password))
+            {
+                MessageBox.Show("Неправильное заполнение данных");
+                return;
+            }
+            else
+            {
+                User autPage = MainWindow.ent.User.ToList().Find(c => c.Login == LoginTxt.Text.Trim() && c.Password == PassPb.Password.Trim());
+                if (autPage != null)
+                {
+                    Go.Content = new ProductsPage();
+                }
+                else
+                {
+                    MessageBox.Show("Проверьте данные");
+                }
+            }
+        }
+
+        private void RegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RegBtn.Visibility = Visibility.Hidden;
+            Go.Content = new RegPage();
+        }
+    }
+}
---
+++ CheckRega/RegPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckRega
+{
+    /// <summary>
+    /// Логика взаимодействия для RegPage.xaml
+    /// </summary>
+    public partial class RegPage : Page
+    {
+        public RegPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(LoginTxt.Text) || String.IsNullOrWhiteSpace(PassPb.Password))
+            {
+                MessageBox.Show("Неправильное заполнение данных");
+                return;
+            }
+            else
+            {
+                User user = new User();
+                var autPage = MainWindow.ent.User.ToList().Find(c => c.Login == LoginTxt.Text.Trim());
+                if (autPage != null)
+                {
+                    MessageBox.Show("Польвозатель уже зарегистрирован");
+                }
+                else
+                {
+                    user.Id_Role = 1;
+                    user.Login = LoginTxt.Text.Trim();
+                    user.Password = PassPb.Password.Trim();
+                    MainWindow.ent.User.Add(user);
+                    MainWindow.ent.SaveChanges();
+                    MessageBox.Show("Вы успешно зарегистрировались");
+                    NavigationService.Navigate( new ProductsPage());      
+                }               
+            }
+        }
+    }
+}
